0fd61a7f4da7494f81e9c59545facff1188efe09
camron <camronmoradi@gmail.com>
2019-02-04 23:31:43 +0100

    Updates Event cards, includes avatar

--- client/we_meet_client/src/components/ui-components/EventsCarousel.js
+++ client/we_meet_client/src/components/ui-components/EventsCarousel.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Carousel from "nuka-carousel";
-import { Card, Box, CardBody, CardFooter, FillButton, OutlineButton } from 'tailwind-react-ui';
+import { Card, Box, CardBody, Col, Row } from 'tailwind-react-ui';
 import moment from 'moment'
 
 class EventsCarousel extends Component {
@@ -12,6 +12,7 @@ class EventsCarousel extends Component {
         id: 1,
         title: 'Hackathon with Craft Academy',
         image: './assets/images/hackathon.jpg',
+        avatar: './assets/images/person.jpg',
         date: 'Tuesday, February 20, 17:30',
         organizer: 'Tom Jones',
         group: 'Craft Academy'
@@ -20,6 +21,7 @@ class EventsCarousel extends Component {
         id: 2,
         title: 'Amphibian workouts',
         image: './assets/images/fit_frogs.jpg',
+        avatar: './assets/images/person.jpg',
         date: 'Tuesday, February 22, 19:00',
         organizer: 'Lazy Bob',
         group: 'Get fit'
@@ -28,6 +30,7 @@ class EventsCarousel extends Component {
         id: 3,
         title: 'Kids game night',
         image: './assets/images/kids_playing.jpg',
+        avatar: './assets/images/person.jpg',
         date: 'Saturday, February 20, 17:30',
         organizer: 'Steve O',
         group: 'Big Moma'
@@ -36,6 +39,7 @@ class EventsCarousel extends Component {
         id: 4,
         title: 'Wining & dining: Italian night',
         image: './assets/images/dining.jpg',
+        avatar: './assets/images/person.jpg',
         date: 'Tuesday, February 22, 19:00',
         organizer: 'Bob Schnell',
         group: 'Food excursions'
@@ -44,6 +48,7 @@ class EventsCarousel extends Component {
         id: 5,
         title: 'Exotic vibes',
         image: './assets/images/exotic_music.jpg',
+        avatar: './assets/images/person.jpg',
         date: 'Tuesday, February 22, 19:00',
         organizer: 'Jonas Gardell',
         group: 'New music'
@@ -52,6 +57,7 @@ class EventsCarousel extends Component {
         id: 6,
         title: 'Craft Academny graduation',
         image: './assets/images/graduation.jpg',
+        avatar: './assets/images/person.jpg',
         date: 'Tuesday, February 22, 19:00',
         organizer: 'Tom Jones',
         group: 'Craft Academy'
@@ -69,7 +75,7 @@ class EventsCarousel extends Component {
             <div >
               <img src={event.image} style={{objectFit:"cover", height:"180px", width:"314px"}}/>
             </div>
-            <div className="top-left" style={{borderRadius:"3px", backgroundColor:"white", width:"50px", padding:"5px"}}>
+            <div className="top-left" style={{borderRadius:"3px", backgroundColor:"white", width:"50px", padding:"5px", border:"0.5px solid rgba(46,62,72,.6)"}}>
               <div style={{fontSize:"20px", fontWeight:"500", lineHeight:"1.1", color:"#CC1F1A"}}>
               {moment(event.date).format("DD")}
               </div>
@@ -77,13 +83,21 @@ class EventsCarousel extends Component {
               {moment(event.date).format("MMM")}
               </div>
             </div>
-            <CardBody>
-              <div>{moment(event.date).format("dddd, MMMM DD, HH:mm")}</div>
-              <div>{event.title}</div>
-              <div>Hosty by {event.organizer}</div>
+            <CardBody style={{textAlign:"left"}}>
+              <div style={{color:"rgba(46,62,72,.6)", fontSize:"16px", fontWeight:"400", lineHeight:"16px"}}>{moment(event.date).format("dddd, MMMM DD, HH:mm")}</div>
+              <div style={{color:"#2e3e48", fontSize:"20px", fontWeight:"600", marginTop:"2px"}}>{event.title}</div>
             </CardBody>
-            <CardFooter>
-            </CardFooter>
+            <Row>
+              <Col w="1/3">
+                <img src={event.avatar} style={{alignment:"left", height:"70px", borderRadius:"80px"}}/>
+              </Col>
+              <Col w="1/3">
+                <div style={{width:"80%", textAlign:"left"}}>
+                  <div>Hosted by {event.organizer}</div>
+                  <div>From {event.group}</div>
+                </div>
+              </Col>
+            </Row>
           </Card>
           <div style={{fontSize: "16px", fontWeight:"600", color:"#2e3e48", marginBottom: "1rem"}}>{event.name}</div>
         </Box>
